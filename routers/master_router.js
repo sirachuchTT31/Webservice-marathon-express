1baeb91a834b339a2f55db1c9fbadafb99d90727
sirachuch <sirachuch.kotuta@gmail.com>
2023-12-17 00:51:50 +0700

    set route master

--- routers/master_router.js
+++ routers/master_router.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { createLocation } = require('../controllers/master_data_controller.js')
-
+const { createLocation, getallLocation } = require('../controllers/master_data_controller.js')
+const { auth_mdw } = require('../middleware/auth.mdw.js')
 // router.get('/master-data/getall', getAll)
-router.post('/master-data/createlocation', createLocation)
+router.post('/master-data/createlocation', auth_mdw, createLocation)
+router.get('/master-data/getall-location', auth_mdw, getallLocation)
 module.exports = router
