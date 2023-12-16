eccbb986df20a9625c8a3541ed840ab341008478
sirachuch <sirachuch.kotuta@gmail.com>
2023-12-17 00:51:44 +0700

    set route register

--- routers/register_running_event_router.js
+++ routers/register_running_event_router.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { register_event, getAll, getbyId } = require('../controllers/register_running_event_controller.js')
+const { create_event, getAll, getbyId, getregbyOrganizer } = require('../controllers/register_running_event_controller.js')
 const { auth_mdw } = require('../middleware/auth.mdw.js')
-router.post('/reg-event/create', auth_mdw, register_event)
+router.post('/reg-event/create', auth_mdw, create_event)
+router.get('/reg-event/get-reg-by-organizer/:id', auth_mdw, getregbyOrganizer)
 router.get('/reg-event/getbyid/:id', getbyId)
 router.get('/reg-event/getall', getAll)
 module.exports = router
